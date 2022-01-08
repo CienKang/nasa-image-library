7c0e8daaf0a90a2a829d999b7b276372d2ed10a4
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2022-01-08 12:39:29 +0530

    Content written

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import HeadBar from "./components/headBar";
+import MainCard from "./components/mainCard";
+
 function App() {
   return (
-    <div>
+    <div className="container">
+      <HeadBar/>
+      <MainCard />
     </div>
   );
 }
---
+++ src/components/headBar.js
@@ -0,0 +1,7 @@
+const HeadBar = () => {
+    return (
+        <h1 align="center">Astronomy Picture of the Day</h1>
+    );
+}
+
+export default HeadBar;
